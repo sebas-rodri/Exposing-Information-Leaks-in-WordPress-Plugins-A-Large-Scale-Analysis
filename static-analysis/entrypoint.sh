be9d50f4ab7151f116d304779f2105c8049f311e
Sebastian Rodriguez <r99@melao.de>
2025-09-19 14:12:45 +0200

    Different Base Image to install Java JDK 21

--- static-analysis/entrypoint.sh
+++ static-analysis/entrypoint.sh
@@ -8,8 +8,12 @@ for i in `seq 0 $NUM_PLUGINS`
 do
     echo "Analyzing plugin number $i"
     python3 download-and-unzip.py plugins_sorted.csv $i
-    semgrep --config semgrep-rules.yml ./plugins > ./results/test-$i.txt
+    #I am actually not sure about excluding the tests directory
+    semgrep --config=semgrep-rules.yml --json --output --include='*.php' --exclude='*/vendor/*' --exclude='*/tests/*' ./results/test-$i.json ./plugins
     rm -rf plugins
 done
+
+#Aggregate semgrep json output
+#aggregate joern output
 
 exec tail -f /dev/null
