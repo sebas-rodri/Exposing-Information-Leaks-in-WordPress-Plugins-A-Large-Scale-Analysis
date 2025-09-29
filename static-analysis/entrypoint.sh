9e876c5595a42b7c3d8edd52045f55a7583fb9b4
Sebastian Rodriguez <r99@melao.de>
2025-09-29 18:49:37 +0200

    cleaner result setup

--- static-analysis/entrypoint.sh
+++ static-analysis/entrypoint.sh
@@ -4,16 +4,22 @@ set -x
 
 pwd
 ls
+#Create DB File
+rm $DB_NAME
+python3 create_duckdb.py $DB_NAME
+python3 parse_yml_to_db.py $DB_NAME
 for i in `seq 0 $NUM_PLUGINS`
 do
     echo "Analyzing plugin number $i"
-    python3 download-and-unzip.py plugins_sorted.csv $i
+    python3 download-unzip-and-infocreation.py plugins_sorted.csv $i
+    slug=$(python3 get_slug.py plugins_sorted.csv $i)
     #I am actually not sure about excluding the tests directory
-    semgrep --config=semgrep-rules.yml --json --output --include='*.php' --exclude='*/vendor/*' --exclude='*/tests/*' ./results/test-$i.json ./plugins
+    semgrep --config=semgrep-rules.yml --json --output --include='*.php' --exclude='*/vendor/*' --exclude='*/tests/*' ./results/$slug/semgrep-$i.json ./plugins
     rm -rf plugins
 done
 
 #Aggregate semgrep json output
 #aggregate joern output
 
+#Keeps Docker container Running
 exec tail -f /dev/null
