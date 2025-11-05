4125e31b74de6499b419cf36df8b47ef1b65f1d8
Sebastian Rodriguez <r99@melao.de>
2025-11-05 19:47:15 +0100

    Added zipping changed files, and logging corresponding event in json

--- dynamic analysis/watcher.sh
+++ dynamic analysis/watcher.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+if [ ! -f "/shared/current_test.txt" ];then
+    touch /shared/current_test.txt
+fi
+
+zip_counter=0 #used for referencing later
 #https://stackoverflow.com/questions/8699293/how-to-monitor-a-complete-directory-tree-for-changes-in-linux
-inotifywait -m -r -e modify,create,delete,move /var/www/html/wp-content --format '%w%f %e %T' --timefmt '%Y-%m-%d %H:%M:%S' | while read file event time
+inotifywait -m -r -e close_write,create,delete,move /var/www/html/wp-content /shared/current_test.txt --format '%w%f %e %T' --timefmt '%Y-%m-%d %H:%M:%S' | while read file event time
 do
+    if [ "$file" = "/shared/current_test.txt" ];then
+        echo "_____________________________________________________";echo "Current Test"; cat "$file" 
+    else #Some file in wp-content changed
+        #json
+        zip_counter=$((zip_counter + 1))
+        jq -c --arg file "$file" --arg event "$event" --arg zip_counter "$zip_counter" \
+            '. + {name_of_changed_file: $file, type_of_operation: $event, zip_counter: $zip_counter}' \
+            /shared/current_test.txt >> /shared/dynamic_test_findings.jsonl
+        #zip contents
+        interface=$(jq -r .interface /shared/current_test.txt)
+        mkdir -p /shared/zip_files/${PLUGIN_SLUG}
+        echo "zip_counter = $zip_counter"
+        (zip /shared/zip_files/${PLUGIN_SLUG}/${interface}_${zip_counter}.zip $file)
+    fi
+
     echo "[$time] File changed: $file (Event: $event)"
     echo "[$time] $event: $file" >> /tmp/file_changes.log
-    cat "$file"
 done
